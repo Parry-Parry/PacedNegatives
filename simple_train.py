dbfb0a3a4a36c334505df08202974814b6add059
Andrew Parry <2393265p@student.gla.ac.uk>
2023-06-11 19:34:28 +0100

    gh

--- simple_train.py
+++ simple_train.py
@@ -101,9 +101,9 @@ def main(dataset : str,
             logging.info(out_ids.shape)
 
             if spl:
-                logits = model(input_ids=inp_ids, labels=out_ids).logits
+                logits = model(input_ids=inp_ids).logits
                 K_loss = torch.sum(v[b]) / K
-                ce = nn.functional.cross_entropy(logits, out_ids, reduction='none')
+                ce = nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), out_ids, reduction='none')
                 loss = (C / torch.sum(v[b])) * torch.sum(ce * v[b]) - K_loss
                 grads = torch.autograd.grad(loss, v[b])
                 v[i] = nn.functional.sigmoid(v[i] - meta_lr * grads[0])
@@ -135,8 +135,8 @@ def main(dataset : str,
                 out_ids = tokenizer(out, return_tensors='pt', padding=True).input_ids.cuda()
 
                 if spl:
-                    logits = model(input_ids=inp_ids, labels=out_ids).logits
-                    loss = (C / torch.sum(v[b])) * torch.sum(nn.functional.cross_entropy(logits, out_ids, reduction='none') * v[b]) - torch.sum(v[b]) / K
+                    logits = model(input_ids=inp_ids).logits
+                    loss = (C / torch.sum(v[b])) * torch.sum(nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), out_ids, reduction='none') * v[b]) - torch.sum(v[b]) / K
                     grads = torch.autograd.grad(loss, v[b])
                     v[i] = nn.functional.sigmoid(v[i] - meta_lr * grads[0])
                     del grads
