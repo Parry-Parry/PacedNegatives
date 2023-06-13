b7abbeb21e12061e046e93d2fd5aea07687fd184
Andrew Parry <2393265p@student.gla.ac.uk>
2023-06-13 13:56:41 +0100

    update eval args

--- eval.py
+++ eval.py
@@ -8,7 +8,7 @@ from pyterrier_t5 import MonoT5ReRanker
 
 models = ['t5baseline16', 't5spl3']
 
-def main(model_dir : str, output_dir : str, corpus : str, eval_name : str):
+def main(model_dir : str, out : str, corpus : str, eval_name : str):
     bm25 = pt.BatchRetrieve.from_dataset(corpus, 'terrier_stemmed_text', wmodel="BM25", metadata=['docno', 'text'])
 
     models = {store : bm25 > MonoT5ReRanker(model=join(model_dir, store, 'model')) for store in models}
@@ -16,9 +16,9 @@ def main(model_dir : str, output_dir : str, corpus : str, eval_name : str):
     dataset = pt.get_dataset(eval_name)
     res = pt.Experiment(models.values(), dataset.get_topics(), dataset.get_qrels(), eval_metrics=["map", "nDCG@10", "mrr"], names = models.keys(), baseline = 0)
     qres = pt.Experiment(models.values(), dataset.get_topics(), dataset.get_qrels(), eval_metrics=["map", "nDCG@10", "mrr"], names = models.keys(), baseline = 0, perquery=True)
-    os.makedirs(output_dir, exist_ok=True)
-    res.to_csv(join(output_dir, "results.csv"))
-    qres.to_csv(join(output_dir, "perqueryresults.csv"))
+    os.makedirs(out, exist_ok=True)
+    res.to_csv(join(out, "results.csv"))
+    qres.to_csv(join(out, "perqueryresults.csv"))
     return "Success!"
 
 if __name__ == '__main__':
