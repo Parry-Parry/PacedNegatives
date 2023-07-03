33e0c81a2e8a004e685dcf9c8993c8ccf76f8844
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-03 16:35:21 +0100

    dobule sanity

--- pacednegatives/dataloader.py
+++ pacednegatives/dataloader.py
@@ -66,6 +66,11 @@ class PairLoader:
                 nx.append(self.format(_q, _n))
                 o_p.append(OUTPUTS[0])
                 o_n.append(OUTPUTS[1])
-        return (px, nx, o_p, o_n)
+
+        print(px)
+        print(nx)
+        print(o_p)
+        print(o_n)
+        return px, nx, o_p, o_n
 
         
