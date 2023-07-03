cfe3ca143115195361c58e197bf93dea0b9eb7be
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-03 17:11:44 +0100

    tidy up repo

--- old/loss.py
+++
@@ -1,8 +0,0 @@
-
-def splloss(x, y, **kwargs):
-    v = kwargs['v']
-    
-    pass
-
-def stdloss(x, y, **kwargs):
-    pass
--- old/util.py
+++
@@ -1,5 +0,0 @@
-from models import *
-
-def init_model(name : str):
-    if name == 'monot5': 
-        return init_t5()
