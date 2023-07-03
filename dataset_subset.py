95e493acb644d31b4e777952dcb38cbc62bcb591
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-03 13:23:50 +0100

    hmm

--- dataset_subset.py
+++ dataset_subset.py
@@ -3,6 +3,7 @@ from fire import Fire
 from paced.util import take_balanced_subset
 
 def main(input_file, output_file, n):
+    print(input_file)
     df = pd.read_json(input_file, orient='records', lines=True)
     df = take_balanced_subset(df, n)
     df.to_json(output_file, orient='records', lines=True)
