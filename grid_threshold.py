30b53ec34a4893f6b264ed84c68607374f156b0d
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-06 13:51:45 +0100

    Fixing kwarg

--- grid_threshold.py
+++ grid_threshold.py
@@ -9,7 +9,7 @@ def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, wandb_project=
     for success_threshold in thresholds:
         for heuristic_step_check in step:
             out = join(out_dir, f'paced_{success_threshold}_{heuristic_step_check}')
-            sp.run(f'python {script} --data {data} --dataset {dataset} --out_dir {out} --batch_size {batch_size} --lr {lr} --success_threshold {success_threshold} --heuristic_step_check {heuristic_step_check} --wandb_project {wandb_project}', shell=True)
+            sp.run(f'python {script} --data {data} --dataset_name {dataset} --out_dir {out} --batch_size {batch_size} --lr {lr} --success_threshold {success_threshold} --heuristic_step_check {heuristic_step_check} --wandb_project {wandb_project}', shell=True)
 
 if __name__ == '__main__':
     Fire(main)
