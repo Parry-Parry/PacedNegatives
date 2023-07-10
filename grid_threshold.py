8011dcf430db1a7db286842a6d3756d4e32277ee
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-10 10:55:07 +0100

    updating grid search to allow for sampling

--- grid_threshold.py
+++ grid_threshold.py
@@ -2,14 +2,17 @@ from fire import Fire
 import subprocess as sp
 from os.path import join
 
-def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, wandb_project=None):
+def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, wandb_project=None, sample=False):
     thresholds = [0.0, 0.25, 0.5, 0.75, 1.0]
     step = [100, 1000, 10000, 100000000000]
+    data_format = 'triples-small-{size}.jsonl'
 
     for success_threshold in thresholds:
         for heuristic_step_check in step:
             out = join(out_dir, f'paced_{success_threshold}_{heuristic_step_check}')
-            sp.run(f'python {script} --data {data} --dataset_name {dataset} --out_dir {out} --batch_size {batch_size} --lr {lr} --success_threshold {success_threshold} --heuristic_step_check {heuristic_step_check} --wandb_project {wandb_project}', shell=True)
+            args = f'python {script} --data {data} --dataset_name {dataset} --out_dir {out} --batch_size {batch_size} --lr {lr} --success_threshold {success_threshold} --heuristic_step_check {heuristic_step_check} --wandb_project {wandb_project}'
+            if sample: args += ' --sample'
+            sp.run(args, shell=True)
 
 if __name__ == '__main__':
     Fire(main)
