c891fa1909290d23bb44710dc72d3ac29c520754
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-13 16:49:48 +0100

    EEE

--- pacednegatives/interp.py
+++ pacednegatives/interp.py
@@ -79,7 +79,7 @@ class InterpWrapper(PacedWrapper):
                 loss = self.main_loop(i)
 
                 if wandb.run is not None:
-                    wandb.log({'loss': loss, 'success_rate' : self.success_rate[-1],'lr': self.scheduler.get_last_lr()[0], 'difficulty': self.difficulty})
+                    wandb.log({'loss': loss, 'lr': self.scheduler.get_last_lr()[0], 'difficulty': self.difficulty})
 
                 self.difficulty = self.interpolate_difficulty(i*self.train_loader.batch_size)
 
