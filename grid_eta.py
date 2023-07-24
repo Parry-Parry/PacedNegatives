24bd3845e116cf2afd65ba583c524dd8a609cc98
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-24 15:08:01 +0100

    eeee

--- grid_eta.py
+++ grid_eta.py
@@ -3,13 +3,15 @@ import subprocess as sp
 from os.path import join
 
 def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, wandb_project=None, sample=False):
-    start = [0.1, 0.2, 0.3, 0.4, 0.5]
+    start = [0.0001, 0.1, 0.2, 0.3, 0.4, 0.5]
+    meta_lrs = [0.0001, 0.0005, 0.001, 0.005, 0.01]
 
-    for s in start:
-        out = join(out_dir, f'paced_{s}')
-        args = f'python {script} --data {data} --dataset_name {dataset} --out_dir {out} --batch_size {batch_size} --lr {lr}  --wandb_project {wandb_project} --eta {s}'
-        if sample: args += ' --sample'
-        sp.run(args, shell=True)
+    for meta_lr in meta_lrs:
+        for s in start:
+            out = join(out_dir, f'paced_{s}_{meta_lr}')
+            args = f'python {script} --data {data} --dataset_name {dataset} --out_dir {out} --batch_size {batch_size} --lr {lr} --meta_lr {meta_lr} --wandb_project {wandb_project} --eta {s}'
+            if sample: args += ' --sample'
+            sp.run(args, shell=True)
 
 if __name__ == '__main__':
     Fire(main)
