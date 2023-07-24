68d6759ef1bbddcd47035ef2831e60a05e4234df
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-24 10:18:21 +0100

    ee

--- grid_eta.py
+++ grid_eta.py
@@ -5,7 +5,7 @@ from os.path import join
 def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, wandb_project=None, sample=False):
     start = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
 
-    for s in start
+    for s in start:
         out = join(out_dir, f'paced_{s}')
         args = f'python {script} --data {data} --dataset_name {dataset} --out_dir {out} --batch_size {batch_size} --lr {lr}  --wandb_project {wandb_project}'
         if sample: args += ' --sample'
