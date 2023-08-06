49c407b593b18443e570aea51bbf8c8be7f25095
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-06 17:25:14 +0100

    more sanity checks

--- pacednegatives/utilities/compute_all_bm25.py
+++ pacednegatives/utilities/compute_all_bm25.py
@@ -27,11 +27,19 @@ def compute_all_bm25(index_path : str,
     all_possible['query'] = all_possible['query_id'].apply(lambda x: queries[x])
 
     topics = all_possible[['query_id', 'query']].rename(columns={'query_id': 'qid'})
+
+    print(f'searching over {len(topics)} topics')
+
     results = model.transform(topics)
 
     results = results[['qid', 'docno']].groupby('qid').agg({'docno': list}).rename(columns={'docno': 'doc_id_b'}).reset_index()
-    results['doc_id_b'] = results['doc_id_b'].apply(lambda x: x[:cutoff][::-1])
+    results['doc_id_b'] = results['doc_id_b'].apply(lambda x: x[::-1])
     negative_dict = results.set_index('qid')['doc_id_b'].to_dict()
+
+    # print docpair dtypes
+    print(docpairs.dtypes)
+    # print negative dict dtypes
+    print(results.dtypes)
     
     docpairs['doc_id_b'] = docpairs['query_id'].apply(lambda x: negative_dict[str(x)])
     docpairs = docpairs.rename(columns={'query_id': 'qid'})
