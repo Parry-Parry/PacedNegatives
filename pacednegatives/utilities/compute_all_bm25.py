28603282a9b5d3f505eebc2b3bfae41e9c74c43b
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-06 20:30:32 +0100

    its insane

--- pacednegatives/utilities/compute_all_bm25.py
+++ pacednegatives/utilities/compute_all_bm25.py
@@ -10,6 +10,7 @@ import re
 def compute_all_bm25(index_path : str, 
                      dataset : str, 
                      output_path : str,
+                     negative_lookup : str = None,
                      threads : int = 1,
                      subsample : int = 100000,
                      cutoff : int = 1000,
@@ -17,31 +18,41 @@ def compute_all_bm25(index_path : str,
    
     os.makedirs(output_path, exist_ok=True)
     clean = lambda x : re.sub(r"[^a-zA-Z0-9¿]+", " ", x)
+    docpairs = pd.DataFrame(ds.docpairs_iter())
 
-    index = PisaIndex.from_dataset(index_path, threads=threads)
-    model = index.bm25(num_results=cutoff, verbose=verbose) 
+    if not negative_lookup:
 
-    #model = pt.BatchRetrieve.from_dataset(index_path, 'terrier_stemmed', wmodel="BM25", verbose=verbose)
+        index = PisaIndex.from_dataset(index_path, threads=threads)
+        model = index.bm25(num_results=cutoff, verbose=verbose) 
 
-    ds = irds.load(dataset)
-    docpairs = pd.DataFrame(ds.docpairs_iter()).drop_duplicates(['query_id'])
-    positive_lookup = docpairs.set_index('query_id')['doc_id_a'].to_dict()
-    queries = pd.DataFrame(ds.queries_iter())
+        ds = irds.load(dataset)
+        
+        queries = pd.DataFrame(ds.queries_iter())
 
-    topics = queries.rename(columns={'query_id': 'qid', 'text' : 'query'})
-    topics['query'] = topics['query'].apply(lambda x: clean(x))
+        topics = queries.rename(columns={'query_id': 'qid', 'text' : 'query'})
+        topics['query'] = topics['query'].apply(lambda x: clean(x))
+        results = model.transform(topics)
 
-    print(f'searching over {len(topics)} topics')
+        results = results[['qid', 'docno']].groupby('qid').agg({'docno': list}).rename(columns={'docno': 'doc_id_b'}).reset_index()
+        results = results[results['doc_id_b'].apply(len) >= cutoff]
+        results['doc_id_b'] = results['doc_id_b'].apply(lambda x: x[::-1])
+        print(f'{len(results)} valid topics found')
+        results.to_json(os.path.join(output_path, f'bm25.{cutoff}.negatives.json'), orient='records')
+    else:
+        results = pd.read_json(negative_lookup, orient='records')
 
-    results = model.transform(topics)
+    negative_lookup = results.set_index('qid')['doc_id_b'].to_dict()
 
-    results = results[['qid', 'docno']].groupby('qid').agg({'docno': list}).rename(columns={'docno': 'doc_id_b'}).reset_index()
-    results = results[results['doc_id_b'].apply(len) >= cutoff]
-    subresults = results.sample(subsample)
-    subresults['doc_id_b'] = subresults['doc_id_b'].apply(lambda x: x[::-1])
-    subresults['doc_id_a'] = subresults['qid'].apply(lambda x: positive_lookup[x])
+    all_topic_ids = docpairs['query_id'].unique().tolist()
+    all_negative_ids = results['qid'].unique().tolist()
+
+    candidates = list(set(all_topic_ids).intersection(set(all_negative_ids)))
+
+    docpairs = docpairs[docpairs['query_id'].isin(candidates)].copy()
+    docpairs = docpairs.sample(subsample)[['query_id', 'doc_id_a']]
+    docpairs['doc_id_b'] = docpairs['query_id'].apply(lambda x: negative_lookup[x])
     
-    subresults.to_json(os.path.join(output_path, f'bm25.{cutoff}.{subsample}.json'), orient='records')
+    docpairs.to_json(os.path.join(output_path, f'bm25.{cutoff}.{subsample}.json'), orient='records')
 
 if __name__ == '__main__':
     Fire(compute_all_bm25)
