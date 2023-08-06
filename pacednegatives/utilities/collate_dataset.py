a99e1bc97904d3dbc0fb32a93cded97c33fadc6e
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-06 12:28:59 +0100

    ree# Please enter the commit message for your changes. Lines starting

--- pacednegatives/utilities/collate_dataset.py
+++ pacednegatives/utilities/collate_dataset.py
@@ -13,7 +13,7 @@ def main(pair_file : str, negative_file : str, output_file : str, cutoff : int =
     
     negative_dict = negatives.set_index('qid').docno.to_dict()
 
-    pairs['doc_id_b'] = pairs['query_id'].apply(lambda x: negative_dict[x])
+    pairs['doc_id_b'] = pairs['query_id'].apply(lambda x: negative_dict[str(x)])
     pairs.to_json(output_file, orient='records')
 
 if __name__ == '__main__':
