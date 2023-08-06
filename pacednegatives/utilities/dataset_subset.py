5b6565c18415b05ba0dc1764f40d431e3b16ef04
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-06 19:57:30 +0100

    oh lawd

--- pacednegatives/utilities/dataset_subset.py
+++ pacednegatives/utilities/dataset_subset.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from fire import Fire
-from pacednegatives.util import take_balanced_subset
 
 def main(input_file : str, output_file : str, n):
-    df = pd.read_json(input_file, orient='records', lines=True)
-    df = take_balanced_subset(df, n)
-    df.to_json(output_file, orient='records', lines=True)
+    df = pd.read_json(input_file, orient='records')
+    df = df.sample(n=n)
+    df.to_json(output_file, orient='records')
 
 if __name__ == '__main__':
     Fire(main)
