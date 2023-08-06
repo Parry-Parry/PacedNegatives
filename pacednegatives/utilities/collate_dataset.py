a2adc9f5a0d4da9990904c2d1f752ac11fe5df0d
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-06 12:27:14 +0100

    idiot

--- pacednegatives/utilities/collate_dataset.py
+++ pacednegatives/utilities/collate_dataset.py
@@ -4,14 +4,14 @@ import pandas as pd
 def main(pair_file : str, negative_file : str, output_file : str, cutoff : int = None):
 
     pairs = pd.read_json(pair_file, orient='records', dtype={'query_id': str, 'doc_id_a': str})
-    negatives = pd.read_json(negative_file, orient='records', dtype={'query_id': str, 'doc_id_b': list})
+    negatives = pd.read_json(negative_file, orient='records', dtype={'qid': str, 'docno': list})
 
     print(negatives.head())
 
     if cutoff is not None:
         pairs = pairs.sample(cutoff)
     
-    negative_dict = negatives.set_index('query_id').doc_id_b.to_dict()
+    negative_dict = negatives.set_index('qid').doc_id_b.to_dict()
 
     pairs['doc_id_b'] = pairs['query_id'].apply(lambda x: negative_dict[x])
     pairs.to_json(output_file, orient='records')
