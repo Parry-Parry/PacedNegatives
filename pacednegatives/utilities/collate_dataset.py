461defdee563245acc4dc2669ef7e2d7f536cf89
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-06 12:28:00 +0100

    dobule idiot

--- pacednegatives/utilities/collate_dataset.py
+++ pacednegatives/utilities/collate_dataset.py
@@ -11,7 +11,7 @@ def main(pair_file : str, negative_file : str, output_file : str, cutoff : int =
     if cutoff is not None:
         pairs = pairs.sample(cutoff)
     
-    negative_dict = negatives.set_index('qid').doc_id_b.to_dict()
+    negative_dict = negatives.set_index('qid').docno.to_dict()
 
     pairs['doc_id_b'] = pairs['query_id'].apply(lambda x: negative_dict[x])
     pairs.to_json(output_file, orient='records')
