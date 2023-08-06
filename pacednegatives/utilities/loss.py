4d73c355d052841e3340d6a1af463f7a5822bd04
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-06 17:08:35 +0100

    silly

--- pacednegatives/utilities/loss.py
+++ pacednegatives/utilities/loss.py
@@ -3,8 +3,8 @@ import torch
 def init_LCEcrossentropy(ignore_index=-100):
     loss_fn = torch.nn.CrossEntropyLoss(ignore_index=ignore_index, reduction='none')
     def LCEcrossentropy(plogits, nlogits, op, on, weights=None, backward=True):
-        pce = loss_fn(plogits.view(-1, plogits.size(-1)), op.view(-1), reduction='none')
-        nce = loss_fn(nlogits.view(-1, nlogits.size(-1)), on.view(-1), reduction='none')
+        pce = loss_fn(plogits.view(-1, plogits.size(-1)), op.view(-1))
+        nce = loss_fn(nlogits.view(-1, nlogits.size(-1)), on.view(-1))
 
         nce = nce.view(-1, nlogits.size(-2))
         nce = torch.mean(nce, dim=1)
