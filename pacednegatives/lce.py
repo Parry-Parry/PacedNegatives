0b44d6864617ef4ab781ee468850c4098840d579
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-07 15:29:05 +0100

    keep everything on cpu

--- pacednegatives/lce.py
+++ pacednegatives/lce.py
@@ -29,7 +29,7 @@ class LCEWrapper(PacedWrapper):
                  use_mean : bool = None) -> None:
         super().__init__(dataset, model_name, batch_size, model_init, tokenizer, lr, ignore_index)
 
-        self.weights = LCEWeights(eta, device=self.device, min=0.+1e-10, max=1.0-1e-10)
+        self.weights = LCEWeights(eta, device=torch.device('cpu'), min=0.+1e-10, max=1.0-1e-10)
         self.logs['eta'] = []
         self.logs['loss'] = {'main': [], 'meta' : []}
         self.logs['difficulty'] = []
@@ -68,13 +68,13 @@ class LCEWrapper(PacedWrapper):
         px, nx, op, on = self.prep_batch(self.train_loader.get_batch(j, self.difficulty))
  
         with torch.no_grad():
-            plogits = self.model(input_ids=px.to(self.device), labels=op).logits
+            plogits = self.model(input_ids=px.to(self.device), labels=op).logits.cpu()
             nlogits = []
             for _batch in batch_iter(nx, n=int(self.batch_size)):
-                nlogits.append(self.model(input_ids=_batch.to(self.device), labels=self.y_neg).logits)
+                nlogits.append(self.model(input_ids=_batch.to(self.device), labels=self.y_neg).logits.cpu())
             nlogits = torch.cat(nlogits, dim=0).view(-1, self.train_loader.n, nlogits[0].size(-1)) # Resolve dimensionality issues
 
-        loss = self.loss_fn(plogits, nlogits, op, on, self.weights)
+        loss = self.loss_fn(plogits, nlogits, op.cpu(), on.cpu(), self.weights)
        
         loss.backward()
         self.meta_optimizer.step()
@@ -90,13 +90,13 @@ class LCEWrapper(PacedWrapper):
     def main_loop(self, j):
         px, nx, op, on = self.prep_batch(self.train_loader.get_batch(j, self.difficulty))
    
-        plogits = self.model(input_ids=px.to(self.device), labels=op).logits
+        plogits = self.model(input_ids=px.to(self.device), labels=op).logits.cpu()
         nlogits = []
         for _batch in batch_iter(nx, n=int(self.batch_size)):
-            nlogits.append(self.model(input_ids=_batch.to(self.device), labels=self.y_neg).logits)
+            nlogits.append(self.model(input_ids=_batch.to(self.device), labels=self.y_neg).logits.cpu())
         nlogits = torch.cat(nlogits, dim=0).view(-1, self.train_loader.n, nlogits[0].size(-1)) # Resolve dimensionality issues
 
-        loss = self.loss_fn(plogits, nlogits, op, on)
+        loss = self.loss_fn(plogits, nlogits, op.cpu(), on.cpu())
         
         loss.backward()
         self.optimizer.step()
