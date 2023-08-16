fedae3e4199638c6e2717926b1010005eb415d96
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-16 09:55:50 +0100

    agh

--- grid_var.py
+++ grid_var.py
@@ -3,7 +3,7 @@ import subprocess as sp
 from os.path import join
 import numpy as np
 
-def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, wandb_project=None, sample=False, mean=False, num_gpus : int = 1):
+def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, meta_lr=None, wandb_project=None, sample=False, mean=False, num_gpus : int = 1):
     #variance = [0.01, 0.025, 0.05, 0.0075, 0.1]
     #N = [2, 4, 8, 16]
     variance = [0.01, 0.05, 0.1]
@@ -16,6 +16,7 @@ def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, wandb_project=
             args = f'python {script} --data {data} --dataset_name {dataset} --out_dir {out} --batch_size {batch_size} --lr {lr} --wandb_project {wandb_project} --eta {start} --var {var} --n {n} --num_gpus {num_gpus}'
             if sample: args += ' --sample'
             if mean: args += ' --use_mean'
+            if meta_lr: args += f' --meta_lr {meta_lr}'
             sp.run(args, shell=True)
 
 if __name__ == '__main__':
