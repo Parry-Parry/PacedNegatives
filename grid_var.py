b0b4d445b179dbb78d525570e7315746a46b01a6
Andrew Parry <2393265p@student.gla.ac.uk>
2023-08-17 10:13:25 +0100

    add to grid

--- grid_var.py
+++ grid_var.py
@@ -3,7 +3,19 @@ import subprocess as sp
 from os.path import join
 import numpy as np
 
-def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, meta_lr=None, wandb_project=None, sample=False, mean=False, num_gpus : int = 1):
+def main(script, 
+         data, 
+         dataset, 
+         out_dir, 
+         batch_size=16, 
+         lr=0.001, 
+         meta_lr=None, 
+         wandb_project=None, 
+         sample=False, 
+         mean=False, 
+         num_gpus : int = 1,
+         num_workers : int = None,
+         max_steps : int = None):
     #variance = [0.01, 0.025, 0.05, 0.0075, 0.1]
     #N = [2, 4, 8, 16]
     variance = [0.01, 0.05, 0.1]
@@ -17,6 +29,8 @@ def main(script, data, dataset, out_dir, batch_size=16, lr=0.001, meta_lr=None, 
             if sample: args += ' --sample'
             if mean: args += ' --use_mean'
             if meta_lr: args += f' --meta_lr {meta_lr}'
+            if num_workers: args += f' --num_workers {num_workers}'
+            if max_steps: args += f' --max_steps {max_steps}'
             sp.run(args, shell=True)
 
 if __name__ == '__main__':
